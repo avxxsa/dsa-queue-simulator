c3ad09f087f6d7fecfe3880f716640515c8158ed
avxxsa <avipsaparajli@gmail.com>
2025-01-29 13:55:18 +0545

    git commit -m "Added basic structure for simulator and generator." git push origin main

---
+++ simulator.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Traffic Simulator Started...\n");
+    return 0;
+}
---
+++ traffic_generator.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Traffic Generator Started...\n");
+    return 0;
+}
