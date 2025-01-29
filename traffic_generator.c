6123dc20f9e2a3dbadb5999a614be247f4b47955
avxxsa <avipsaparajli@gmail.com>
2025-01-29 17:41:35 +0545

    git commit -m ""

--- traffic_generator.c
+++ traffic_generator.c
@@ -4,3 +4,52 @@ int main() {
     printf("Traffic Generator Started...\n");
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")  // Link with Winsock library
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+#define BUFFER_SIZE 1024
+
+int main() {
+    WSADATA wsa;
+    SOCKET sock;
+    struct sockaddr_in serv_addr;
+    char *message = "Vehicle arrives at the junction";
+
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
+        printf("WSAStartup failed. Error Code: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("Socket creation failed. Error Code: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    // Connect to simulator
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR) {
+        printf("Connection failed. Error Code: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    // Send vehicle data
+    send(sock, message, strlen(message), 0);
+    printf("Vehicle data sent to simulator\n");
+
+    // Cleanup
+    closesocket(sock);
+    WSACleanup();
+
+    return 0;
+}
