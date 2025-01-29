e1757418ec08f8a3d242e99c0a145068da7335be
avxxsa <avipsaparajli@gmail.com>
2025-01-29 17:45:52 +0545

    commit -m "Added basic socket setup for simulator and generator."

--- traffic_generator.c
+++ traffic_generator.c
@@ -7,49 +7,45 @@ int main() {
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <winsock2.h>
-
-#pragma comment(lib, "ws2_32.lib")  // Link with Winsock library
+#include <arpa/inet.h>
+#include <unistd.h>
 
 #define PORT 8080
 #define SERVER_IP "127.0.0.1"
 #define BUFFER_SIZE 1024
 
 int main() {
-    WSADATA wsa;
-    SOCKET sock;
+    int sock = 0;
     struct sockaddr_in serv_addr;
     char *message = "Vehicle arrives at the junction";
 
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
-        printf("WSAStartup failed. Error Code: %d\n", WSAGetLastError());
-        return 1;
-    }
-
     // Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        printf("Socket creation failed. Error Code: %d\n", WSAGetLastError());
-        return 1;
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
     }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    // Convert IP address
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        return -1;
+    }
 
     // Connect to simulator
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR) {
-        printf("Connection failed. Error Code: %d\n", WSAGetLastError());
-        return 1;
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        return -1;
     }
 
     // Send vehicle data
     send(sock, message, strlen(message), 0);
     printf("Vehicle data sent to simulator\n");
 
-    // Cleanup
-    closesocket(sock);
-    WSACleanup();
+    // Close socket
+    close(sock);
 
     return 0;
 }
